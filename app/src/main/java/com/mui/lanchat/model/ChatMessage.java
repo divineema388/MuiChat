b1f9589910afe005129c71f247510fad76332ae4
divineema388 <divineemaasuquo@gmail.com>
2025-06-05 01:24:38 +0100

    Update ChatMessage.java

--- app/src/main/java/com/mui/lanchat/model/ChatMessage.java
+++ app/src/main/java/com/mui/lanchat/model/ChatMessage.java
@@ -4,13 +4,21 @@ import com.google.gson.Gson;
 
 public class ChatMessage {
     private String senderIp;
+    private String senderName; // <--- NEW FIELD
     private String message;
     private long timestamp;
 
-    public ChatMessage(String senderIp, String message) {
+    // Updated constructor to include senderName
+    public ChatMessage(String senderIp, String senderName, String message) {
         this.senderIp = senderIp;
+        this.senderName = senderName; // <--- ASSIGN NEW FIELD
         this.message = message;
         this.timestamp = System.currentTimeMillis();
+    }
+
+    // Getter for senderName
+    public String getSenderName() {
+        return senderName;
     }
 
     public String getSenderIp() {
@@ -27,20 +35,13 @@ public class ChatMessage {
 
     // Convert ChatMessage object to JSON string
     public String toJson() {
-        return new Gson().toJson(this);
+        Gson gson = new Gson();
+        return gson.toJson(this);
     }
 
-    // Convert JSON string to ChatMessage object
-    public static ChatMessage fromJson(String jsonString) {
-        return new Gson().fromJson(jsonString, ChatMessage.class);
-    }
-
-    @Override
-    public String toString() {
-        return "ChatMessage{" +
-               "senderIp='" + senderIp + '\'' +
-               ", message='" + message + '\'' +
-               ", timestamp=" + timestamp +
-               '}';
+    // Convert JSON string back to ChatMessage object
+    public static ChatMessage fromJson(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, ChatMessage.class);
     }
 }
